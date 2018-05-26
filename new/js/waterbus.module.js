fa4874443b761aa43f9038f35239c5c245482795
Thang Tran <thangtd1993@gmail.com>
2018-05-26 15:18:46 +0700

    dang lam tang ve waterbus

--- new/js/waterbus.module.js
+++ new/js/waterbus.module.js
@@ -1,6 +1,8 @@
 var isRoundWaterBus = false;
+var numberTicket = 1;
 
 $(document).ready(function() {
+    $('#numberTicket').val(numberTicket + " vé");
     $('#bookingWaterBus #isRound').change(function () {
         if($(this).is(":checked")){
             isRoundWaterBus = true;
@@ -13,6 +15,18 @@ $(document).ready(function() {
             $('#divStartPoint').removeClass('col-6');
             $('#divStartPoint').addClass('col-12');
         }
+    });
+    
+    $('#upTicket').click(function () {
+        numberTicket++;
+        $('#numberTicket').val(numberTicket + " vé");
+    });
 
+    $('#downTicket').click(function () {
+        if(numberTicket > 1) {
+            numberTicket--;
+        }
+
+        $('#numberTicket').val(numberTicket + " vé");
     });
 });
