9e4e2d9e6d6424e20dad2505c4f6c63e13e28e49
Thang Tran <thangtd1993@gmail.com>
2018-06-01 22:56:01 +0700

    saigon river

--- new/js/saigonriver.module.js
+++ new/js/saigonriver.module.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     var numberTicket = 1;
+    var depatureDate = $('#booingSaigonRiver #depatureDate');
+
+    depatureDate.datepicker({
+        dateFormat: 'dd/mm/yy',
+        minDate: 0,
+    }).datepicker("setDate", new Date());
 });
