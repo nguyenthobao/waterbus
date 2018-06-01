7780288dba94a1a1513dce3c5a889edce66249f3
Thang Tran <thangtd1993@gmail.com>
2018-06-02 03:03:40 +0700

    dang lam watertaxi

--- new/js/watertaxi/watertaxi.module.js
+++ new/js/watertaxi/watertaxi.module.js
@@ -56,5 +56,69 @@ $(document).ready(function() {
 
         initEndMarker(latEnd, longEnd, pointNameEnd);
     });
+    
+    $('#seachWaterTaxi').click(function () {
+        $.alert({
+            title: 'Thông báo',
+            type: 'green',
+            typeAnimated: true,
+            content: 'Bạn đang sử dụng dịch vụ Saigon Watertaxi. Phí mở cửa là 270.000 VNĐ/lượt cho nhóm tối đa 6 người. ' +
+            'Từ người tiếp theo, giá vé là 60.000 VNĐ/người/lượt',
+            buttons: {
+                ok: {
+                    text: 'Đồng ý'
+                }
+            },
+            onClose: function () {
+                getSchedule(startPoint, endPoint, vehicleType, depatureDate);
+            }
+        });
+        return false;
+    });
+
+    /*Lấy danh sách các chuyến*/
+    function getSchedule(startPoint, endPoint, vehicleType, date) {
+        var dateAr = date.val().split('/');
+        var newDate = dateAr[0] + '-' + dateAr[1] + '-' + dateAr[2];
+
+        var startPointName = splitPointName(startPoint.find(':selected').text());
+        var endPointName = splitPointName(endPoint.find(':selected').text());
+
+        var routeName = startPointName + " - " + endPointName;
+
+        $('.route-name').text(routeName);
+
+        $('#bookingWaterTaxi .booing-form').hide(300);
+        $('#bookingWaterTaxi .list-schedule').show(300);
+
+        $('.schedule-list').html('<div class="loading"></div>');
+
+        $.ajax({
+            type: "POST",
+            url: "https://anvui.vn/listSchedule2",
+            data: {
+                startPoint: startPoint.val(),
+                endPoint: endPoint.val(),
+                date: newDate,
+                companyId: systemId,
+                vehicleTypeId: vehicleType.val(),
+                page: 0,
+                count: 1000
+            },
+            success: function (result) {
+                buildSchedulListOneWay(result);
+            }
+        });
+    }
+
+    /*Lấy tên bến tàu*/
+    function splitPointName(pointName) {
+        var res = pointName.split(" ");
+        var newName = '';
+        for (i = 3; i < res.length; i++) {
+            newName += res[i] + " ";
+        }
+        return newName;
+    }
 
 });
