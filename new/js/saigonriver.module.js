99d0d1e34aa682a1146274a504d5f4d854816ef6
Thang Tran <thangtd1993@gmail.com>
2018-06-01 23:48:31 +0700

    saigon river

--- new/js/saigonriver.module.js
+++ new/js/saigonriver.module.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     var numberTicket = 1;
     var depatureDate = $('#booingSaigonRiver #depatureDateRiver');
 
+    var vehicleType = $('#booingSaigonRiver #vehicleType');
+
     depatureDate.datepicker({
         dateFormat: 'dd/mm/yy',
         minDate: 0,
@@ -20,4 +22,50 @@ $(document).ready(function() {
         }
         $('#booingSaigonRiver #numberTicket').val(numberTicket + " vé");
     });
+
+    $('#seachSaigonRiver').click(function () {
+        if(depatureDate.val() === "") {
+            $.alert({
+                title: 'Cảnh báo!',
+                type: 'red',
+                typeAnimated: true,
+                content: 'Chưa chọn ngày đi',
+            });
+            return false;
+        }
+
+        getSchedule(vehicleType, depatureDate);
+    });
+
+    /*Lấy danh sách các chuyến*/
+    function getSchedule(vehicleType, date) {
+        var dateAr = date.val().split('/');
+        var newDate = dateAr[0] + '-' + dateAr[1] + '-' + dateAr[2];
+
+        var startPoint = "P03k1Iq7vRvdDS";
+        var endPoint = "P03k1IqIku8fXQ";
+
+        $('.schedule-list').html('<div class="loading"></div>');
+
+        $('#booingSaigonRiver .booing-form').hide(300);
+        $('#booingSaigonRiver .list-schedule').show(300);
+
+        $.ajax({
+            type: "POST",
+            url: "https://anvui.vn/listSchedule2",
+            data: {
+                startPoint: startPoint,
+                endPoint: endPoint,
+                date: newDate,
+                companyId: systemId,
+                vehicleTypeId: vehicleType.val(),
+                page: 0,
+                count: 1000
+            },
+            success: function (result) {
+                console.log(result);
+                buildSchedulListOneWay(result);
+            }
+        });
+    }
 });
