c45fbba5a027a964386d64f1358320b385434248
Thang Tran <thangtd1993@gmail.com>
2018-06-01 23:06:01 +0700

    saigon river

--- new/js/saigonriver.module.js
+++ new/js/saigonriver.module.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
         dateFormat: 'dd/mm/yy',
         minDate: 0,
     }).datepicker("setDate", new Date());
+
+    $('#booingSaigonRiver #numberTicket').val(numberTicket + " vé");
+
+    $('#booingSaigonRiver #upTicket').click(function () {
+        numberTicket++;
+        $('#booingSaigonRiver #numberTicket').val(numberTicket + " vé");
+    });
+
+    $('#booingSaigonRiver  #downTicket').click(function () {
+        if(numberTicket > 1) {
+            numberTicket--;
+        }
+        $('#booingSaigonRiver #numberTicket').val(numberTicket + " vé");
+    });
 });
