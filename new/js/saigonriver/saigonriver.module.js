71908680a1e9dee9bca4fa8ffef439d0153af865
Thang Tran <thangtd1993@gmail.com>
2018-06-02 15:14:44 +0700

    gioi han so luong ve duoc dat

--- new/js/saigonriver/saigonriver.module.js
+++ new/js/saigonriver/saigonriver.module.js
@@ -254,6 +254,13 @@ $(document).ready(function() {
 
             $('#booingSaigonRiver .ticket-info-list').html(ticketHtml);
         }
+
+        $('.phoneNumber').on('keypress keyup blur', function () {
+            $(this).val($(this).val().replace(/[^\d].+/, ""));
+            if ((event.which < 48 || event.which > 57)) {
+                event.preventDefault();
+            }
+        });
     });
 
     function payment(dataPayment) {
